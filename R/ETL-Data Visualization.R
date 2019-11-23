abbcc282c13b7761d2712bebbae7b01c8830bb94
soloemoon <57546826+soloemoon@users.noreply.github.com>
2019-11-23 14:13:43 -0500

    Update ETL-Data Visualization.R

--- R/ETL-Data Visualization.R
+++ R/ETL-Data Visualization.R
@@ -271,7 +271,7 @@ eco.barline.plot <-function(df,x,bar,line,bar.color,line.color,title,caption,sub
 
 eco.bar.plot <-function(df,x,y,fill,y.title,x.title,title,caption,labels,subtitle, legend.pos,bar.position,xaxis.text.size,yaxis.text.size,title.size,caption.size,legend.size){
   x <- ifelse(missing(x),'date',x)
-  bar.position <- ifelse(missing(bar.position),'dodge')
+  bar.position <- ifelse(missing(bar.position),'',bar.position)
   subtitle <- ifelse(missing(subtitle),'',subtitle)
   y.title <- ifelse(missing(y.title),'', y.title)
   x.title <- ifelse(missing(x.title),'', x.title)
