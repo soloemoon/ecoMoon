52a463e520fb6a6628125a27b9eea8a563987a7b
soloemoon <57546826+soloemoon@users.noreply.github.com>
2020-01-01 15:42:10 -0500

    Update eco.transform.R

--- R/eco.transform.R
+++ R/eco.transform.R
@@ -10,6 +10,9 @@ eco.transform <-function(df, transformation, lags, index.date){
   df.trans <-within(df, rm('recession'))
   }
 
+# Determine class of object - dataframe or xts.
+if(is.data.frame(df.trans) == 'TRUE'){
+
 # Non Annualized Growth Rates
 if(transformation == '% change'){
 
@@ -59,4 +62,59 @@ if(transformation == '% change'){
     return(df.trans)
 
   }
+}else if(is.xts(df.trans) == 'TRUE'){
+
+  xts.dates <-index(df.trans)
+
+  # Non Annualized Growth Rates
+  if(transformation == '% change'){
+
+    df.trans <-xts(vapply(df.trans, function(x){
+      if(is.numeric(x)){x / lag(x, lags) - 1
+      }else{x}}, FUN.VALUE = numeric(nrow(df.trans))), order.by = xts.dates )
+
+    return(df.trans)
+
+    # Change
+  }else if(transformation == 'difference'){
+
+    df.trans <-xts(vapply(df.trans, function(x){
+      if(is.numeric(x)){x - lag(x,lags)
+      }else{x}}, FUN.VALUE = numeric(nrow(df.trans))), order.by = xts.dates )
+
+    return(df.trans)
+
+    # Log Transformation
+  }else if(transformation == 'log'){
+
+    df.trans <-log(df.trans[ , unlist(lapply(df.trans, is.numeric))])
+
+    df.trans <-xts(vapply(df.trans, function(x){
+      if(is.numeric(x)){x - lag(x,lags)
+      }else{x}}, FUN.VALUE = numeric(nrow(df.trans))), order.by = xts.dates )
+
+    return(df.trans)
+
+    # Monthly Annualized % Change
+  } else if(transformation == 'annualize monthly'){
+
+    df.trans <-xts(vapply(df.trans, function(x){
+      if(is.numeric(x)){((x/lag(x,lags))^12 - 1)
+      }else{x}}, FUN.VALUE = numeric(nrow(df.trans))), order.by = xts.dates )
+
+    return(df.trans)
+
+    # Quarterly Annualized % Change
+  }else if(transformation == 'annualize quarterly'){
+
+    df.trans <-xts(vapply(df.trans, function(x){
+      if(is.numeric(x)){((x/lag(x,lags))^4 - 1)
+      }else{x}}, FUN.VALUE = numeric(nrow(df.trans))), order.by = xts.dates )
+
+    return(df.trans)
+
+  }
 }
+
+}
+
