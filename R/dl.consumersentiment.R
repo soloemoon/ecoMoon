8d80bd25c4ac1e0bbae366952027ecf810ab26d3
Solomon Moon <57546826+soloemoon@users.noreply.github.com>
2020-05-21 12:46:35 -0400

    Updated read excel formula
    File format is .xls so changed formula to read.xls

--- R/dl.consumersentiment.R
+++ R/dl.consumersentiment.R
@@ -2,7 +2,7 @@ dl.consumer.sentiment <-function(save.location, start.date){
 
 download.file('http://www.sca.isr.umich.edu/files/tbmics.xls',save.location, method = 'auto', mode = 'wb',quiet=TRUE) 
 
-consumer.sentiment <-read.xlsx(save.location,1, startRow = 4, colIndex = 1:3) %>% na.omit()
+consumer.sentiment <-read.xls(save.location,1, startRow = 4, colIndex = 1:3) %>% na.omit()
 consumer.sentiment$date <-mdy(paste(consumer.sentiment$DATE.OF.SURVEY, '01',consumer.sentiment$NA.))
 consumer.sentiment <-consumer.sentiment[ ,c('INDEX.OF.CONSUMER.SENTIMENT', 'date')]
 row.names(consumer.sentiment) <-consumer.sentiment$date
